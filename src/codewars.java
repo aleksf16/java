c1ba5cd20a50651803e3c6869bd10c99e828a2fe
aleksf16 <aleksf16@mail.ru>
2020-11-10 00:11:47 +0300

    первый коммит

---
+++ src/codewars.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class codewars {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = 0;
+        System.out.println("Введите число");
+        a = in.nextInt();
+        System.out.println("Влад сосал хуй " + a + " раз");
+    }
+}
